bbe466b02cbd2deb64c100e7b9ab18cfe0f6dda9
Satviki Pathak <satvikipathak@gmail.com>
2021-11-29 05:06:36 -0700

    Add more tests. Remove attributes not supported by HTML 5.

--- tests/test_server.py
+++ tests/test_server.py
@@ -11,18 +11,10 @@ def test_get_all_tickets(app, client):
     response = client.get('/tickets')
     assert response.status_code == 200
 
+def test_error_on_get_all_tickets(app, client):
+    response = client.get('/tickets/')
+    assert response.status_code != 200
 
-def test_response_data_for_get_one_ticket(app, client):
-    #Test ticket details for ticket id 1 are as per expected
-    response = client.get('/tickets/1')
-    assert response.status_code == 200
-    print(response)
-    print(response.json())
-    data = response.json()
-    to_display = data['results']
-    assert to_display[0]['id'] == 1
-    assert to_display[0]['status'] == 'open'
-    assert to_display[0]['priority'] == 'normal'
-    assert to_display[1]['name'] == 'The Customer'
-    assert to_display[2]['name'] == 'Satviki Sanjay Pathak'
-
+def test_error_on_nonexistent_api(app, client):
+    response = client.get('/people')
+    assert response.status_code != 200
